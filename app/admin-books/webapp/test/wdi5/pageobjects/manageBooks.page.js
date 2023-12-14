493fb91f1f2f1b793470ef8c750ff886488521a5
Leonardo Britz <leonardo.britz@sap.com>
2023-12-14 05:43:06 +0000

    Added test configuration

--- app/admin-books/webapp/test/wdi5/pageobjects/manageBooks.page.js
+++ app/admin-books/webapp/test/wdi5/pageobjects/manageBooks.page.js
@@ -1,9 +1,23 @@
 module.exports = {
-  /**
-    * define actions for the page object
-  */
-
-  /**
-   * define assertions for the page object
-   */
+  iClickOnCreateNewBook: async () => {
+    await browser.asControl({
+    	selector: {
+            id: "preview::BooksListTarget--fe::table::Books::LineItem::StandardAction::Create",
+            interaction: {
+                    idSuffix: "BDI-content"
+            }
+    }}).press();
+  },
+  theBookListContains: async (sBook) => {
+    const visible = await browser.asControl({
+    	selector: {
+            controlType: "sap.m.Link",
+            viewName: "sap.fe.templates.ListReport.ListReport",
+            viewId: "preview::BooksListTarget",
+            properties: {
+                    text: sBook
+            }
+    }}).getVisible();
+    expect(visible).toEqual(sBook);
+  }
 }
--- app/admin-books/webapp/test/wdi5/pageobjects/newBook.page.js
+++ app/admin-books/webapp/test/wdi5/pageobjects/newBook.page.js
@@ -1,9 +1,69 @@
 module.exports = {
-        /**
-         * define actions for the page object
-         */
-
-        /**
-         * define assertions for the page object
-         */
+        iEnterTitle: async () => {
+                await browser.asControl({
+                        selector: {
+                                id: "preview::BooksDetailsTarget--fe::EditableHeaderForm::EditableHeaderTitle::Field-edit",
+                                interaction: {
+                                        idSuffix: "inner"
+                                }
+                        }
+                }).enterText("test");
+        },
+        iSelectGenre: async () => {
+                await browser.asControl({
+                        selector: {
+                                id: "preview::BooksDetailsTarget--fe::FormContainer::FieldGroup::General::FormElement::DataField::genre_ID::Field-edit-inner-vhi"
+                        }
+                }).press();
+                await browser.asControl({
+                        selector: {
+                        controlType: "sap.m.Text",
+                        viewName: "sap.fe.templates.ObjectPage.ObjectPage",
+                        viewId: "preview::BooksDetailsTarget",
+                        bindingPath: {
+                                path: "/Genres(10)",
+                                propertyPath: "name"
+                        },
+                        searchOpenDialogs: true
+                }}).press();
+        },
+        iSelectAuthor: async () => {
+                await browser.asControl({
+                        selector: {
+                        id: "preview::BooksDetailsTarget--fe::FormContainer::FieldGroup::General::FormElement::DataField::author_ID::Field-edit-inner-vhi"
+                }}).press();
+                await browser.asControl({
+                        selector: {
+                        controlType: "sap.fe.macros.controls.FieldWrapper",
+                        viewName: "sap.fe.templates.ObjectPage.ObjectPage",
+                        viewId: "preview::BooksDetailsTarget",
+                        searchOpenDialogs: true,
+                        ancestor: {
+                                id: "preview::BooksDetailsTarget--fe::FormContainer::FieldGroup::General::FieldValueHelp::author_ID::Dialog::qualifier::::Table-innerTable-rows-row2",
+                                searchOpenDialogs: true
+                        }
+                }}).press();
+        },
+        iPressCreate: async () => {
+                await browser.asControl({
+                        selector: {
+                        id: "preview::BooksDetailsTarget--fe::FooterBar::StandardAction::Save",
+                        interaction: {
+                                idSuffix: "BDI-content"
+                        }
+                }}).press();
+        },
+        iSeeEditButton: async () => {
+                const enabled = await browser.asControl({
+                        selector: {
+                        id: "preview::BooksDetailsTarget--fe::StandardAction::Edit"
+                }}).getEnabled();
+                expect(enabled).toBeTruthy();
+        },
+        iNavigateBack: async () => {
+                await browser.asControl({
+                        selector: {
+                        id: "backBtn"
+                }}).press();
+        }
 };
